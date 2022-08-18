6ea9cff7097d4e71893a00b5f29d2fec718d4100
nivorako <nivo.rakoto@yahoo.fr>
2022-08-18 17:20:28 +0200

    set up html structure, header logo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log('hello word')
+class App{
+    constructor(){
+        this.wrapper = document.getElementById('body')
+    }
+
+    main(){
+        const main = `
+            <h1> Hello world by JS </h1>
+        `
+        this.wrapper.innerHTML = main;
+    }
+}
+
+const app = new App();
+app.main();
