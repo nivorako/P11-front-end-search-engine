83a88ccdde07649f15c601e0ba44080189e3e8ee
nivorako <nivo.rakoto@yahoo.fr>
2022-09-05 21:08:14 +0200

    try to fetch

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
+class Api {
+    constructor(url){
+        this._url = url;
+    }
+    async get(){
+        return fetch(this._url)
+            .then(res => res.json())
+            .then(res => res.data)
+            .catch(err => console.log('an error occurs', err))
+    }
+}
+
 class App{
     constructor(){
-        this.wrapper = document.getElementById('main')
+        this.wrapper = document.getElementById('main');
+        this.recipesApi = new Api("/recipes.js")
     }
 
-    main(){
-        const main = `
-            <h1> Hello world by JS </h1>
-        `
-        this.wrapper.innerHTML = main;
+    async main(){
+
+       const recipes = await this.recipesApi.get()
+       console.log("recipes: ", recipes)
     }
 }
 
 const app = new App();
-// app.main();
+ app.main();
